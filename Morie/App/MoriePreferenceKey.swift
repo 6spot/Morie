f32a1765432a6f67e4a185638974cc26105dee35
spot <30459129+6spot@users.noreply.github.com>
2026-09-22 12:13:00 +0800

    Centralize lightweight runtime preference keys

---
+++ Morie/App/MoriePreferenceKey.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+enum MoriePreferenceKey {
+    static let inputRefinementEnabled =
+        "inputRefinementEnabled"
+    static let dictionarySuggestionsEnabled =
+        "dictionaryCorrectionSuggestionsEnabled"
+    static let expressionLearningEnabled =
+        "expressionProfileLearningEnabled"
+    static let soundFeedbackEnabled =
+        "capture.soundFeedback.enabled"
+}
